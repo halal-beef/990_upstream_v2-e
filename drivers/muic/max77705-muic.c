2b9a736895274a54e59e8491af9bbc5e230489ef
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2025-06-25 20:59:37 +0100

    muic: max77705-muic: Silence logspam
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

--- drivers/muic/max77705-muic.c
+++ drivers/muic/max77705-muic.c
@@ -2075,12 +2075,14 @@ static void max77705_muic_print_reg_log(struct work_struct *work)
 	max77705_muic_read_reg(i2c, MAX77705_USBC_REG_VDM_INT_M, &status[10]);
 	max77705_muic_read_reg(pmic_i2c, MAX77705_PMIC_REG_INTSRC_MASK, &status[11]);
 
+#if defined(CONFIG_EXYNOS_LOG_CLEANUP_REVERT)
 	pr_info("%s USBC1:0x%02x, USBC2:0x%02x, BC:0x%02x, CC0:0x%x, CC1:0x%x, PD0:0x%x, PD1:0x%x attached_dev:%d\n",
 		__func__, status[0], status[1], status[2], status[3], status[4], status[5], status[6],
 		muic_data->attached_dev);
 	pr_info("%s UIC_INT_M:0x%x, CC_INT_M:0x%x, PD_INT_M:0x%x, VDM_INT_M:0x%x, PMIC_MASK:0x%x, WDT:%d, POR:%d\n",
 		__func__, status[7], status[8], status[9], status[10], status[11],
 		muic_data->usbc_pdata->watchdog_count, muic_data->usbc_pdata->por_count);
+#endif
 
 	schedule_delayed_work(&(muic_data->debug_work),
 		msecs_to_jiffies(60000));
